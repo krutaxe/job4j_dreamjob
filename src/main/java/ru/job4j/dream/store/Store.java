93a1a00b5555a62b5ea2e0a356171b7c3871fccd
krutaxe <krutaxe@mail.ru>
2022-05-21 23:25:30 +0300

    4. candicates.jsp - список кандидатов. [#276522]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -1,8 +1,10 @@
 package ru.job4j.dream.store;
 
+import ru.job4j.dream.model.Candidate;
 import ru.job4j.dream.model.Post;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Map;
@@ -12,6 +14,7 @@ public class Store {
     private static final Store INST = new Store();
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job",
@@ -20,13 +23,21 @@ public class Store {
                 "Инженер по автомат. тестированию, Средний (Middle) • Git • Java • Appium • Selenium ", LocalDate.now()));
         posts.put(3, new Post(3, "Senior Java Job",
                 "SMSAPI - это команда, которая отвечает за разработку верификационных сервисов VK. ", LocalDate.now()));
+        candidates.put(1, new Candidate(1, "Junior Java"));
+        candidates.put(2, new Candidate(2, "Middle Java"));
+        candidates.put(3, new Candidate(3, "Senior Java"));
     }
 
     public static Store instOf() {
         return INST;
     }
 
-    public Collection<Post> findAll() {
+    public Collection<Post> findAllPosts() {
         return posts.values();
     }
+
+    public Collection<Candidate> findAllCandidates() {
+        return candidates.values();
+    }
+
 }
