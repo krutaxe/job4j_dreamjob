273955c3fa6fa90e6b4819053e8d6c3bc1f78298
krutaxe <krutaxe@mail.ru>
2022-09-08 06:55:56 +0300

    2. HttpSession [#504864 #340894]

--- src/main/java/ru/job4j/dreamjob/util/SessionHttp.java
+++ src/main/java/ru/job4j/dreamjob/util/SessionHttp.java
@@ -6,13 +6,12 @@ import ru.job4j.dreamjob.model.User;
 import javax.servlet.http.HttpSession;
 
 public class SessionHttp {
-    public static Model getSessionUser(Model model, HttpSession session) {
+    public static void getSessionUser(Model model, HttpSession session) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
             user = new User();
             user.setName("Гость");
         }
         model.addAttribute("userSession", user);
-        return model;
     }
 }
